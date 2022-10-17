2b95792a30a81396d9efd811e5f110764926e4a1
A J <aj@debian-BULLSEYE-live-builder-AMD64>
2022-10-18 00:30:36 +0200

    P 10-12

--- P1.fsx
+++ P1.fsx
@@ -1,11 +1,23 @@
 #load "P0.fsx"
 open P0
 
-//Problem 11: Only elements with duplicates are transferred as (N E) lists others are simply copied.
+// Problem 11: Only elements with duplicates are transferred as (N E) lists others are simply copied.
 type Encoding = 
-    Single of Char
-    Multiple of int*char 
+    | Single of char
+    | Multiple of int*char 
 
 let encodeModified ls = 
-    encode ls 
+    encode ls |> List.map (fun (n,e) -> if n=1 then Single e else Multiple (n,e)) 
 
+
+
+// Problem 12: reverse of problem 11
+let decodeModified ls = 
+    ls 
+    |> List.map (fun x -> match x with 
+                          | Single c -> List.init 1 (fun _ -> c)
+                          | Multiple (n,e) -> List.init n (fun _ -> e)) 
+    |> List.reduce (fun x y -> x@y) 
+
+
+// Problem 13: L
